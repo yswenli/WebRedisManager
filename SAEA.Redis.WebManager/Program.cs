fd031116e1f5860c96eaf2a096414d13fb70a3a2
wenli <wenguoli_520@qq.com>
2018-08-07 01:16:01 +0800

    上传图片~

--- SAEA.Redis.WebManager/Program.cs
+++ SAEA.Redis.WebManager/Program.cs
@@ -13,13 +13,15 @@ namespace SAEA.Redis.WebManager
     {
         static void Main(string[] args)
         {
-            ConsoleHelper.Title = "SAEA.Redis.WebManager";
-
-            ConsoleHelper.WriteLine("服务已启动，按回车结束...");
+            ConsoleHelper.Title = "WebRedisManagerService";
 
             MvcApplication mvcApplication = new MvcApplication(10240, 3000);
 
             mvcApplication.Start();
+
+            ConsoleHelper.WriteLine("WebRedisManager服务已启动，按回车结束...");
+
+            ConsoleHelper.WriteLine("http://localhost:39654/html/index.html，按回车结束...");
 
             Process.Start("http://localhost:39654/html/index.html");
 
