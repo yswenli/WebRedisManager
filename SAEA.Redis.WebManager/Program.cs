b92642a31b9e56bc64c95a17db26a474b5798aaa
wenli <wenguoli_520@qq.com>
2018-09-08 01:49:41 +0800

    更新底层缓存方式，修复连接复用bug~

--- SAEA.Redis.WebManager/Program.cs
+++ SAEA.Redis.WebManager/Program.cs
@@ -15,7 +15,7 @@ namespace SAEA.Redis.WebManager
         {
             ConsoleHelper.Title = "WebRedisManagerService";
 
-            MvcApplication mvcApplication = new MvcApplication(false, false, 10240, 1000);
+            MvcApplication mvcApplication = new MvcApplication(false, false, 10240, 1);
 
             mvcApplication.Start();
 
