51ec3f635ed3fb67478a64a27521614aa128ec03
wenli <wenguoli_520@qq.com>
2018-11-16 18:02:57 +0800

    update

--- SAEA.Redis.WebManager/Program.cs
+++ SAEA.Redis.WebManager/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SAEA.Common;
 using SAEA.MVC;
+using System;
 using System.Diagnostics;
 
 namespace SAEA.WebRedisManager
@@ -14,9 +15,10 @@ namespace SAEA.WebRedisManager
 
             mvcApplication.Start();
 
+
             ConsoleHelper.WriteLine("WebRedisManager已启动");
 
-            ConsoleHelper.WriteLine("请在浏览器上打开：http://localhost:39654/");      
+            ConsoleHelper.WriteLine("请在浏览器上打开：http://localhost:39654/");
 
             ConsoleHelper.WriteLine("回车退出服务...");
 
