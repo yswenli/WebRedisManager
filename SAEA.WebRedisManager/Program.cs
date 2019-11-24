e8583e981bb50c1dbe64308825daae37b8d1c5c2
yswenli <yswenli@outlook.com>
2019-11-24 23:59:00 +0800

    update to v5.2.1.6

--- SAEA.WebRedisManager/Program.cs
+++ SAEA.WebRedisManager/Program.cs
@@ -7,7 +7,7 @@ namespace SAEA.WebRedisManager
     {
         static void Main(string[] args)
         {
-            ConsoleHelper.Title = "WebRedisManager v5.2.0.1";
+            ConsoleHelper.Title = "WebRedisManager v5.2.1.6";
 
             var config = SAEAMvcApplicationConfigBuilder.Read();
 
